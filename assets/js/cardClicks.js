6c2d5e0f1a38b6b3b587dd8ad0449f639aa1b736
treguv <vladislavtregubov00@gmail.com>
2020-11-05 01:15:05 -0800

    made links clickable

---
+++ assets/js/cardClicks.js
@@ -0,0 +1,16 @@
+// handle the clicks for all the card elements
+$("#fly-high").on("click", function () {
+  open("https://treguv.github.io/fly-high/");
+});
+
+$("#pdf-highlight").on("click", function () {
+  open("https://github.com/treguv/dubhacks2020");
+});
+
+$("#weather-dashboard").on("click", function () {
+  open("https://treguv.github.io/weather-dashboard/");
+});
+
+$("#run-buddy").on("click", function () {
+  open("https://treguv.github.io/run-buddy/");
+});
