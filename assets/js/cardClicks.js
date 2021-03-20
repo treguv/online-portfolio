59da4444f924077dd300156eedf0e24fce2d4b63
treguv <vladislavtregubov00@gmail.com>
2021-03-20 15:21:35 -0700

    Updated colors, added project, added gifs to cards, changed font.

--- assets/js/cardClicks.js
+++ assets/js/cardClicks.js
@@ -1,25 +1,25 @@
 // handle the clicks for all the card elements
-$("#fly-high").on("click", function () {
-  open("https://treguv.github.io/fly-high/");
-});
+// $("#fly-high").on("click", function () {
+//   open("https://treguv.github.io/fly-high/");
+// });
 
-$("#pdf-highlight").on("click", function () {
-  open("https://github.com/treguv/dubhacks2020");
-});
+// $("#pdf-highlight").on("click", function () {
+//   open("https://github.com/treguv/dubhacks2020");
+// });
 
-$("#weather-dashboard").on("click", function () {
-  open("https://treguv.github.io/weather-dashboard/");
-});
+// $("#weather-dashboard").on("click", function () {
+//   open("https://treguv.github.io/weather-dashboard/");
+// });
 
-$("#run-buddy").on("click", function () {
-  open("https://treguv.github.io/run-buddy/");
-});
-$("#note-taker").on("click", function () {
-  open("https://github.com/treguv/note-taker/tree/main");
-});
-$("#stuck-inside").on("click", function () {
-  open("http://www.stuckinside.tech");
-});
+// $("#run-buddy").on("click", function () {
+//   open("https://treguv.github.io/run-buddy/");
+// });
+// $("#note-taker").on("click", function () {
+//   open("https://github.com/treguv/note-taker/tree/main");
+// });
+// $("#stuck-inside").on("click", function () {
+//   open("http://www.stuckinside.tech");
+// });
 
 //Make the nav bar elements change size based on screen size
 function resizeNav() {
